ed40a2ffc3c3a3ac515ecf3b9fdcb5ef7ceb4511
Zoe Leullier <git@zoe.pw>
2025-03-07 17:58:47 -0300

    Optimize store

--- configuration.nix
+++ configuration.nix
@@ -13,6 +13,16 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.configurationLimit = 10;
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 1w";
+  };
+
+  nix.settings.auto-optimise-store = true;
+
 
   networking.hostName = "laptop"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
